98a5e7cc090978bfc58db97b26b580a8bf4fd0c8
jasonkim0809 <kimjason0809@gmail.com>
2025-03-18 23:39:00 -0400

    directions enum

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/enums/Actions.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/enums/Actions.java
@@ -2,9 +2,9 @@ package ca.mcmaster.se2aa4.island.teamXXX.enums;
 
 public enum Actions {
     turn_left, turn_right, fly, echo_left, echo_right, echo_forward, scan, stop;
-    public void enumtest(){
+    public void enumtest(Directions direction){
         if (this == turn_left){
-            System.out.println("rueninf lwdr");
+            
         }
         else if (this == turn_right){
             // stuff here for turning right
--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/enums/Directions.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/enums/Directions.java
@@ -1,6 +1,62 @@
 package ca.mcmaster.se2aa4.island.teamXXX.enums;
-import org.json.JSONObject;
 
-public class Directions {
-    
+public enum Directions {
+    N,E,W,S;
+    public Directions turn_right(){
+        Directions new_direction = this;
+        switch (new_direction) {
+            case N:
+                new_direction = E;
+                break;
+            case E:
+                new_direction = S;
+                break;
+            case W:
+                new_direction = N;
+                break;
+            case S:
+                new_direction = W;
+                break;
+        };
+        return new_direction;
+    }
+    public Directions turn_left(){
+        Directions new_direction = this;
+        switch (new_direction) {
+            case N:
+                new_direction = W;
+                break;
+            case E:
+                new_direction = N;
+                break;
+            case W:
+                new_direction = S;
+                break;
+            case S:
+                new_direction = E;
+                break;
+        };
+        return new_direction;
+    }
+
+    @Override
+    public String toString(){
+        String new_str = "";
+        switch (this) {
+            case N:
+                new_str = "N";
+                break;
+            case E:
+                new_str = "E";
+                break;
+            case W:
+                new_str = "W";
+                break;
+            case S:
+                new_str = "S";
+                break;
+        };
+
+        return new_str;
+    }
 }
